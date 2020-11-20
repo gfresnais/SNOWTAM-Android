44f1ba7381d74c534ab6423735d3c4290be62dd2
David HENG <davidheng.pw@gmail.com>
2020-11-20 15:04:57 +0100

    SplashScreenV1

--- app/src/main/java/com/ensim/snowtam/MainActivity.java
+++ app/src/main/java/com/ensim/snowtam/MainActivity.java
@@ -2,18 +2,43 @@ package com.ensim.snowtam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ensim.snowtam.Controller.Controller;
 
 public class MainActivity extends AppCompatActivity {
 
     private Controller controller;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         controller = new Controller(getAssets());
+        //Fait disparaître la barre d'action
+        getSupportActionBar().hide();
+        
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomePageActivity();
+            }
+        });
+
+
+
     }
+
+    public void openHomePageActivity(){
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
