79de1e272e1f65d7d843b53be9a81535590f704c
David HENG <davidheng.pw@gmail.com>
2020-11-26 11:16:39 +0100

    SplashScreenV2 animation and no invisible button

--- app/src/main/java/com/ensim/snowtam/MainActivity.java
+++ app/src/main/java/com/ensim/snowtam/MainActivity.java
@@ -4,42 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.ensim.snowtam.Controller.Controller;
 import com.ensim.snowtam.View.HomePageActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000; //5 s de splash screen
 
-    private Button button;
+    Animation top_animation;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        //Fait disparaître la barre d'action
-        getSupportActionBar().hide();
-        
+        //Animations
+        top_animation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
 
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        //Hooks
+        image = findViewById(R.id.logo);
+
+        image.setAnimation(top_animation);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                openHomePageActivity();
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                startActivity(intent);
+                finish();
             }
-        });
-
+        }, SPLASH_SCREEN);
 
 
     }
 
-    public void openHomePageActivity(){
-        Intent intent = new Intent(this, HomePageActivity.class);
-        startActivity(intent);
-        finish();
-    }
 
 }
